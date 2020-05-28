7bfbccd1098b3a4387b8a7cbeb4a79aeb8e3a564
kojiyuu-123 <yusuke.kojima@pascalia.co.jp>
2020-05-28 16:25:56 +0900

    commit_5_28

---
+++ firstapp_CodeIgniter-3.1.11/application/models/Users_model.php
@@ -0,0 +1,48 @@
+<?php
+class Users_model extends CI_Model {
+
+    public function __construct()
+    {
+        $this->load->database();
+    }
+    
+    public function get_users()
+	{
+    	$query = $this->db->get('users');
+    	return $query->result_array();
+	}
+	
+	public function update_user($user_id)
+	{
+	    $data = array(
+	        'first_name' => $this->input->post('first_name'),
+	        'last_name' => $this->input->post('last_name'),
+	        'email' => $this->input->post('email')
+	    );
+
+	    $this->db->where('id', $user_id);
+	    return $this->db->update('users', $data);
+	}
+
+	public function create_user()
+	{
+	    $this->load->helper('url');
+
+	    $data = array(
+	        'first_name' => $this->input->post('first_name'),
+	        'last_name' => $this->input->post('last_name'),
+	        'email' => $this->input->post('email')
+	    );
+
+	    return $this->db->insert('users', $data);
+	}
+
+	public function get_user($user_id)
+	{
+	    $query = $this->db->where('id', $user_id)->get('users');
+	    return $query->row();
+	}
+
+}
+
+?>
